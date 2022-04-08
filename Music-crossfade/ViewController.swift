908f5f3378b76b2c7a0b73d1e5af254de9b1eb7e
SemjonG <simon.gerasimov@gmail.com>
2022-04-08 15:18:07 +0300

    Initial Commit

---
+++ Music-crossfade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music-crossfade
+//
+//  Created by mac on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
